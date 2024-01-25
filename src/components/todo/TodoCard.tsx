35300b08a6fba8d3e43a3c7bd16b99340d69b882
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-01-25 20:24:18 +0600

    TodoCard and todoContainer create done

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,9 +1,18 @@
+import { Button } from "../ui/button";
+
 const TodoCard = () => {
-    return (
-        <div>
-            I am form Todo Card
-        </div>
-    );
+  return (
+    <div className="bg-white rounded-md flex justify-between items-center p-3">
+      <input type="checkbox"></input>
+      <p>Todo title</p>
+      <p>Time</p>
+      <p>Description</p>
+      <div className="space-x-4">
+        <Button>Delete</Button>
+        <Button>Edit</Button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoCard;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,28 +1,23 @@
 import { Button } from "../ui/button";
+import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
-    return (
-        <div>
-            {/* Add and filter todo  */}
-            <div>
-                <Button>Add todo</Button>
-                <Button>Filter</Button>
-            </div>
+  return (
+    <div>
+      {/* Add and filter todo  */}
+      <div className="space-x-3 pb-3">
+        <Button>Add todo</Button>
+        <Button>Filter</Button>
+      </div>
 
-            <div className="bg-red-500 w-full h-screen rounded-xl p-5">
-                <div>
-                    <input type="checkbox"></input>
-                    <p>Todo title</p>
-                    <p>Time</p>
-                    <p>Description</p>
-                    <div>
-                        <Button>Delete</Button>
-                        <Button>Edit</Button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-2">
+        {/* <div className="bg-white text-red-500 p-5 rounded-md text-center">
+          <p className="text-2xl font-bold">There is no task pending</p>
+        </div> */}
+        <TodoCard></TodoCard>
+      </div>
+    </div>
+  );
 };
 
 export default TodoContainer;
