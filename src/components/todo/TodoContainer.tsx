d1fce3c7389b7d98b669505ccd9f497e558bbbc2
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-01-25 20:48:05 +0600

    Add background color with gradient and re style Todo page

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -5,16 +5,31 @@ const TodoContainer = () => {
   return (
     <div>
       {/* Add and filter todo  */}
-      <div className="space-x-3 pb-3">
-        <Button>Add todo</Button>
-        <Button>Filter</Button>
+      <div className="flex justify-between pb-3">
+        <Button className="bg-primary-gradient text-lg font-bold">Add todo</Button>
+        <Button className="bg-primary-gradient text-lg font-bold">Filter</Button>
       </div>
 
-      <div className="bg-red-500 w-full h-full rounded-xl p-5 space-y-2">
+      <div className="bg-primary-gradient w-full h-full rounded-xl p-[5px] space-y-2">
         {/* <div className="bg-white text-red-500 p-5 rounded-md text-center">
           <p className="text-2xl font-bold">There is no task pending</p>
         </div> */}
-        <TodoCard></TodoCard>
+        <div className="bg-white rounded-lg p-2 space-y-3 ">
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+          <TodoCard></TodoCard>
+        </div>
       </div>
     </div>
   );
