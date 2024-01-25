ec82485d431cf2799164bb4b861708977ce1c90a
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2024-01-25 16:39:49 +0600

    Todo with redux intia setup done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-7xl  flex justify-center items-center  font-bold  text-red-500">
+       empty-react-ts-app
+    </h1>
+    <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
